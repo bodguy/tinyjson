4af4bef74b60cb3642067f43df7cce1e07dd7aa8
alsrudgns99 <alsrudgns99@zuminternet.com>
2019-12-09 13:10:29 +0900

    add missing files

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
   tinyjson::Value value;
   StopWatch watch;
   std::string json;
-  bool res = tinyjson::read_file("../sample2.json", json);
+  bool res = tinyjson::read_file("../sample5.json", json);
   if (!res) return -1;
   watch.start();
   res = tinyjson::parseJson(value, json);
